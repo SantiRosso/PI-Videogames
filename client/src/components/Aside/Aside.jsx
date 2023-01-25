58a409df57aa0955d753350d8e47271bca018cd0
SantiRosso <santiagorosso@live.com.ar>
2023-01-25 02:58:53 -0300

    Aside styles fixed

--- client/src/components/Aside/Aside.jsx
+++ client/src/components/Aside/Aside.jsx
@@ -30,9 +30,9 @@ const Aside = ({setPage, genreA, setGenreA, setInput, handleRating, handleSort, 
     }
 
     return(
-        <div>
+        <div className={s.containter}>
             <h1 className={s.title}>Filter by</h1>
-            <h3 className={s.container}>Genres</h3>
+            <h3 className={s.subtitle}>Genres</h3>
             <div className={s.genres}>
                 {
                     genres?.map((e,i) => {
@@ -50,23 +50,23 @@ const Aside = ({setPage, genreA, setGenreA, setInput, handleRating, handleSort, 
                 </div>
             </div>  
             <h3 className={s.order}>Order by</h3>
-                        <div className={s.orderDiv}>
-                            <select className={s.select} name="Sort" onChange={handleSort}>
-                                <option value="sort">Alphabet</option>
-                                <option value="asc">A-Z</option>
-                                <option value="des">Z-A</option>
-                            </select>
-                            <select className={s.select} name="Rating" onChange={handleRating}>
-                                <option value="rating">Rating</option>
-                                <option value="men">Minor-Major</option>
-                                <option value="may">Major-Minor</option>
-                            </select>
-                        </div>
-                        <div className={s.divReset}>
-                            <button className={s.reset} onClick={handleReset}>Reset filters</button>
-                        </div>
-                        <Pagination input={input} setInput={setInput} page={page} setPage={setPage} max={max}/>
-                        <Footer/>          
+            <div className={s.orderDiv}>
+                <select className={s.select} name="Sort" onChange={handleSort}>
+                    <option value="sort">Alphabet</option>
+                    <option value="asc">A-Z</option>
+                    <option value="des">Z-A</option>
+                </select>
+                <select className={s.select} name="Rating" onChange={handleRating}>
+                    <option value="rating">Rating</option>
+                    <option value="men">Minor-Major</option>
+                    <option value="may">Major-Minor</option>
+                </select>
+            </div>
+            <div className={s.divReset}>
+                <button className={s.reset} onClick={handleReset}>Reset filters</button>
+            </div>
+            <Pagination input={input} setInput={setInput} page={page} setPage={setPage} max={max}/>
+            <Footer/>          
         </div>
     )
 }
