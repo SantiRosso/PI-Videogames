4c3beca66e291f89909517e3c7a9e44cd2478f16
SantiRosso <santiagorosso@live.com.ar>
2023-01-30 11:32:18 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-// axios.defaults.baseURL = process.env.REACT_APP_API;
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API;
+// axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
