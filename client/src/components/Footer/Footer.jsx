7de55f1711d390d3eb40d1205802f813794a4df7
SantiRosso <santiagorosso@live.com.ar>
2023-06-23 15:19:40 -0300

    Footer, Card and Detail styles

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -8,11 +8,11 @@ import s from "./Footer.module.css"
 const Footer = () => {
     return(
         <div className={s.div}>
-            <a href="https://www.linkedin.com/in/santiago-rosso-421484227/" title="linkedin"><img src={linkedin} width="30rm" alt="linkedin" /></a>
-            <a href="https://github.com/SantiRosso" title="github"><img src={github} width="30rm" alt="linkedin" /></a>
-            <a href="https://www.instagram.com/santi.rosso/" title="instagram"><img src={instagram} width="30rm" alt="linkedin" /></a>
-            <a href="https://santirosso.github.io/Portfolio-1/" title="portfolio"><img src={portfolio} width="30rm" alt="linkedin" /></a>
-            {/* <a href="" title="whatsapp"> */}<img src={whatsapp} width="30rm" alt="whatsapp" />{/* </a> */}
+            <a href="https://www.linkedin.com/in/santiago-rosso-421484227/" title="linkedin" target="blanck"><img src={linkedin} width="30rm" alt="linkedin" className={s.icon} /></a>
+            <a href="https://github.com/SantiRosso" title="github" target="blanck"><img src={github} width="30rm" alt="linkedin" className={s.icon} /></a>
+            <a href="https://www.instagram.com/santi.rosso/" title="instagram" target="blanck"><img src={instagram} width="30rm" alt="linkedin" className={s.icon} /></a>
+            <a href="https://portfolio-olive-three-24.vercel.app/" title="portfolio" target="blanck"><img src={portfolio} width="30rm" alt="linkedin" className={s.icon} /></a>
+            {/* <a href="" title="whatsapp"> */}<img src={whatsapp} width="30rm" alt="whatsapp" className={s.icon} />{/* </a> */}
         </div>    
     )
 }
