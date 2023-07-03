3833ea023a52e9b6c641a3bde81aea248a9fba66
SantiRosso <santiagorosso@live.com.ar>
2023-07-03 13:13:16 -0300

    Nav responsive. feat: Drawer

--- client/src/components/Switch/Switch.jsx
+++ client/src/components/Switch/Switch.jsx
@@ -1,9 +1,9 @@
 import "./Switch.css";
 
-const Switch = () => {
+const Switch = ({click}) => {
     return(
         <div>
-            <input hidden="" class="check-icon" id="check-icon" name="check-icon" type="checkbox"/>
+            <input onClick={click} hidden="true" class="check-icon" id="check-icon" name="check-icon" type="checkbox"/>
             <label class="icon-menu" for="check-icon">
                 <div class="bar bar--1"></div>
                 <div class="bar bar--2"></div>
